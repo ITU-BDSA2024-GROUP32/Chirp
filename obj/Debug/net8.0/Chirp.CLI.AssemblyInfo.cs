1d2fd611f6e40e07bb4f5c1e4b55fd93bc25c5e3
Viea2000 <viea@itu.dk>
2024-09-04 15:53:50 +0200

    1

--- obj/Debug/net8.0/Chirp.CLI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Chirp.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e53104756a9ed0841babbbb05d957917e0903f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a4c1ebfbb879db08228d67413a45784a3a1e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
